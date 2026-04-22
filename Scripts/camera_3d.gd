5b0e9d1729451542f3edb8c65d880d42d322b381
Basil <seagar16@kingshigh.school.nz>
2026-04-22 14:48:05 +1200

    Player and Test area

---
+++ Scripts/camera_3d.gd
@@ -0,0 +1,11 @@
+extends Camera3D
+
+@export var target: Node3D
+@export var offset := Vector3(0, 5, 3)
+@export var speed := 10.0
+
+func _process(delta):
+	if target:
+		var goal = target.global_position + offset
+		global_position = global_position.lerp(goal, speed * delta)
+		look_at(target.global_position, Vector3.UP)
