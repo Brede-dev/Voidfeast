7ac2da25ba53a64dcedf0737d0f5fc376f5d3848
Brede-dev <liadal15@kingshigh.school.nz>
2026-05-01 14:22:18 +1200

    Food added
    Removed herb

---
+++ Scripts/food.gd
@@ -0,0 +1,18 @@
+extends Area3D
+var food = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_entered(area: Area3D) -> void:
+	food += 1
+	print(food)
+	queue_free()
+	pass # Replace with function body.
