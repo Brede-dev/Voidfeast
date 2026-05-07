dd502217edf1aa03bed7e106908cda667a39df34
Basil <seagar16@kingshigh.school.nz>
2026-05-07 12:11:11 +1200

    Food edit

--- Scripts/food.gd
+++ Scripts/food.gd
@@ -1,11 +1,14 @@
 extends Area3D
 const ROT_SPEED = 2
+var score = 0
+
 
 func _on_body_entered(body: Node3D) -> void:
+	if body is not Player:
+		return
+	score += 1
+	print(score)
 	queue_free()
 
 func _process(delta: float) -> void:
 	rotate_y(deg_to_rad(ROT_SPEED))
-
-	if has_overlapping_bodies():
-		queue_free()
