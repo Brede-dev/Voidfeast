39953250030e8fa6ceb739223fcfbffbeda855eb
Brede-dev <liadal15@kingshigh.school.nz>
2026-05-08 13:19:15 +1200

    Added score, food, and beacon functionality

--- Scripts/food.gd
+++ Scripts/food.gd
@@ -1,13 +1,12 @@
 extends Area3D
 const ROT_SPEED = 2
-var score = 0
+
 
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is not Player:
 		return
-	var newscore = score + 1
-	print(newscore)
+	GameManager.add_score(1)
 	queue_free()
 
 func _process(delta: float) -> void:
