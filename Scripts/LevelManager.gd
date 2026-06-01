83f62cb823532009b2a5ffc2422f43778d7b062a
alebor28 <alebor28@kingshigh.school.nz>
2026-06-02 10:51:59 +1200

    Score carry over between levels

--- Scripts/LevelManager.gd
+++ Scripts/LevelManager.gd
@@ -1,6 +1,5 @@
 extends Node
 
-func _ready():
-	var food_count = get_tree().get_node_count_in_group("Food")
-	GameManager.level_target = food_count
-	GameManager.score = 0
+func _ready() -> void:
+	var food_count: int = get_tree().get_node_count_in_group("Food")
+	GameManager.start_level(food_count)
