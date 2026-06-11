ae95c030b2baba1905d06bdd5850ec7cce0d3634
Basil <seagar16@kingshigh.school.nz>
2026-06-11 12:25:38 +1200

    Update LevelManager.gd

--- Scripts/LevelManager.gd
+++ Scripts/LevelManager.gd
@@ -2,4 +2,4 @@ extends Node
 
 func _ready() -> void:
 	var food_count: int = get_tree().get_node_count_in_group("Food")
-	GameManager.start_level(food_count)
+	GameManager.start_level(food_count)#goldenfo	odcount
