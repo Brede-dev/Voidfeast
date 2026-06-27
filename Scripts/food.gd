2b952c611fe49f8e54a7f7fb5d72d1ec572e8a72
Bagel19 <seagar16@kingshigh.school.nz>
2026-06-28 00:10:19 +1200

    Adding more code

--- Scripts/food.gd
+++ Scripts/food.gd
@@ -12,9 +12,12 @@ func _on_body_entered(body: Node3D) -> void:
 	if body is not Player:
 		return
 	
+	
 	if is_collected:
 		return
+		
 	is_collected = true
+	
 	
 	if is_golden:
 		GameManager.add_score(1, is_golden)
@@ -23,7 +26,26 @@ func _on_body_entered(body: Node3D) -> void:
 	await get_tree().create_timer(0.1).timeout
 	hide()
 
-
-
 func _process(delta: float) -> void:
 	rotate_y(deg_to_rad(ROT_SPEED))
+
+func apply_gold_shader() -> void:
+	is_golden = true
+	
+	#var shader: Shader = preload()
+	var material: ShaderMaterial = ShaderMaterial.new()
+	material.shader = Shader
+	apply_material_repulsive(self, material)
+
+func apply_material_reppulsive(node: Node, material: Material) -> void:
+	if node is MeshInstance3D:
+		node.set_surface_override_material(0, material)
+	for child in node.get_children():
+		apply_material_recursive(child, material)
+
+func respawn_with_gold_shader() -> void:
+	is_collected = false
+	global_position = original_position
+	show()
+	$CollisionShape3D.disabled = false
+	apply_gold_shader()
