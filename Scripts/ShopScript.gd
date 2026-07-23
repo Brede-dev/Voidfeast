5321fd44bb09d31b95aa159732fe36f4165c2fe4
Basil <seagar16@kingshigh.school.nz>
2026-07-23 14:58:50 +1200

    I made it so that each upgrade has their own unique upgrade

--- Scripts/ShopScript.gd
+++ Scripts/ShopScript.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	var speed_upgrade_card: Control = sell_thing.instantiate()
 	speed_upgrade_card.item_id = "speed_upgrade"
 	speed_upgrade_card.cost = 10
-	speed_upgrade_card.item_texture = preload("res://Assets/icon.svg")
+	speed_upgrade_card.item_texture = preload("res://Assets/SportShoes (1).png")
 	if speed_upgrade_card.title_label:
 		speed_upgrade_card.title_label.text = "Speed Boost"
 	card_container.add_child(speed_upgrade_card)
@@ -16,7 +16,7 @@ func _ready() -> void:
 	var double_jump_upgrade_card: Control = sell_thing.instantiate()
 	double_jump_upgrade_card.item_id = "double_jump_upgrade"
 	double_jump_upgrade_card.cost = 10
-	speed_upgrade_card.item_texture = preload("res://Assets/icon.svg")
+	double_jump_upgrade_card.item_texture = preload("res://Assets/DoubleJump (1).png")
 	if double_jump_upgrade_card.title_label:
 		double_jump_upgrade_card.title_label.text = "Double Jump"
 	card_container.add_child(double_jump_upgrade_card)
@@ -24,7 +24,7 @@ func _ready() -> void:
 	var high_jump_upgrade_card: Control = sell_thing.instantiate()
 	high_jump_upgrade_card.item_id = "high_jump_upgrade"
 	high_jump_upgrade_card.cost = 10
-	speed_upgrade_card.item_texture = preload("res://Assets/icon.svg")
+	high_jump_upgrade_card.item_texture = preload("res://Assets/NinjaBelt (1).png")
 	if high_jump_upgrade_card.title_label:
 		high_jump_upgrade_card.title_label.text = "High Jump"
 	card_container.add_child(high_jump_upgrade_card)
@@ -32,7 +32,7 @@ func _ready() -> void:
 	var high_collecting_range_upgrade_card: Control = sell_thing.instantiate()
 	high_collecting_range_upgrade_card.item_id = "higher_collection_range_upgrade"
 	high_collecting_range_upgrade_card.cost = 10
-	speed_upgrade_card.item_texture = preload("res://Assets/icon.svg")
+	high_collecting_range_upgrade_card.item_texture = preload("res://Assets/GiftMagnet (1).png")
 	if high_collecting_range_upgrade_card.title_label:
 		high_collecting_range_upgrade_card.title_label.text = "Larger Collection Range"
 	card_container.add_child(high_collecting_range_upgrade_card)
