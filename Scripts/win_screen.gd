dc224d818127a5ba91b9e5ff464570bd3be3c5a1
Basil <seagar16@kingshigh.school.nz>
2026-07-30 12:53:56 +1200

    test

--- Scripts/win_screen.gd
+++ Scripts/win_screen.gd
@@ -3,7 +3,7 @@ extends Control
 
 
 func _on_restart_button_pressed() -> void:
-	FadeTransition.fade_to_scene("res://Scenes/Level1.tscn")
+	FadeTransition.fade_to_scene("res://Scenes/LevelSelection.tscn")
 
 func _on_menu_button_pressed() -> void:
 	FadeTransition.fade_to_scene("res://Scenes/main_menu.tscn")
