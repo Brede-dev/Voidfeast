22692c8edc9cd5b27afa675d173f6767071116ba
Basil <seagar16@kingshigh.school.nz>
2026-08-10 14:56:57 +1200

    I added a model for our player

--- Scripts/Play.gd
+++ Scripts/Play.gd
@@ -47,16 +47,20 @@ func _physics_process(delta: float) -> void:
 		if times_jumped == 0:
 			velocity.y = JUMP_VELOCITY * GameManager.jump_upgrade
 			times_jumped = 1
+			get_node("Box/AnimationPlayer").play("Armature|Jump_001")
 		
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY * GameManager.jump_upgrade
+		get_node("Box/AnimationPlayer").play("Armature|Jump_001")
 		
 	if Input.is_action_just_pressed("Jump") and not is_on_floor():
 		if times_jumped == 1 and GameManager.is_item_purchased("double_jump_upgrade"):
 			velocity.y = JUMP_VELOCITY
 			times_jumped = 2
+			get_node("Box/AnimationPlayer").play("Armature|Jump_001")
 	# Get the input direction
 	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
+	
 	
 	# Direction is relative to the player's basis (which is rotated by Mouse X)
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -67,6 +71,7 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
+		get_node("Box/AnimationPlayer").play("Armature|Walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, current_speed)
 		velocity.z = move_toward(velocity.z, 0, current_speed)
