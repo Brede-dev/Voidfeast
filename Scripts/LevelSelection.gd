3e0ec2f6fa6bebae9e512a6064454f7f1306149b
Basil <seagar16@kingshigh.school.nz>
2026-08-18 14:57:15 +1200

    Update LevelSelection.gd

--- Scripts/LevelSelection.gd
+++ Scripts/LevelSelection.gd
@@ -2,16 +2,16 @@ extends Control
 
 func _on_level_1_pressed() -> void:
 	$Click.play()
-	FadeTransition.fade_to_scene("res://Scenes/Level1.tscn")
+	FadeTransition.fade_to_scene("res://Scenes/Game/Level1.tscn")
 
 func _on_level_2_pressed() -> void:
 	$Click.play()
-	FadeTransition.fade_to_scene("res://Scenes/Level2.tscn")
+	FadeTransition.fade_to_scene("res://Scenes/Game/Level2.tscn")
 
 func _on_level_3_pressed() -> void:
 	$Click.play()
-	FadeTransition.fade_to_scene("res://Scenes/Level3.tscn")
+	FadeTransition.fade_to_scene("res://Scenes/Game/Level3.tscn")
 
 func _on_main_menu_pressed() -> void:
 	$Click.play()
-	FadeTransition.fade_to_scene("res://Scenes/MainMenu.tscn")
+	FadeTransition.fade_to_scene("res://Scenes/Screens/MainMenu.tscn")
