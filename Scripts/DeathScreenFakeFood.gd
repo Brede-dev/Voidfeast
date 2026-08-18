8ca0d391182fd4c9f65ced2601bbf1079cf95e31
Basil <seagar16@kingshigh.school.nz>
2026-08-18 15:35:30 +1200

    Adjustments P3

--- Scripts/DeathScreenFakeFood.gd
+++ Scripts/DeathScreenFakeFood.gd
@@ -8,7 +8,8 @@ func _process(_delta: float) -> void:
 	# Keep the mouse unlocked the entire time we are on this screen
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
-func _on_restart_button_pressed() -> void:
+
+func _on_level_selection_button_pressed() -> void:
 	GameManager.food_collected = 0
 	GameManager.total_food_owned = 0
 	GameManager.level_score = 0
